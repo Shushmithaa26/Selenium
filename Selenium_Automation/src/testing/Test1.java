c5337100c6b03e8eadf85288ccd0907f35131fba
Shuhsmitha <Shuhsmitha@DESKTOP-DRUE99P.attlocal.net>
2019-06-11 19:18:26 -0700

    Added script in Test1 class

--- Selenium_Automation/src/testing/Test1.java
+++ Selenium_Automation/src/testing/Test1.java
@@ -3,6 +3,10 @@ package testing;
 public class Test1 {
 	
 	public static void main(String[] args) {
+		System.out.println("ASdfdgsdgsdg");
+		System.out.println("ASdfdgsdgsdg");
+		System.out.println("ASdfdgsdgsdg");
+		System.out.println("ASdfdgsdgsdg");
 		
 	}
 
